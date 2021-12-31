15a1d7afc0bf66e265124258902593b4a4794d0a
dogurasu <dugs134@gmail.com>
2021-12-30 18:05:28 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import sys
+
+def main():
+    data = dict({
+        'outstandingShares': None,
+        'cash': None,
+        'cashBurn': None,
+        'debt': None,
+        'revenue': None,
+        'income': None
+    })
+
+    with open('filing-details.html', 'r') as f:
+        # locate outstandingShares
+
+        # locate and calculate cash and cashBurn
+
+        # locate debt
+
+        # locate revenue
+        
+        # locate income
+    
+    # print data
+    
+
+if __name__ == "__main__":
+    main()
